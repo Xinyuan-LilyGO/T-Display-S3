84e8604072100ac7d1aabb26e0819de61537c085
lewishe <lewishe@outlook.com>
2023-03-21 10:09:43 +0800

    Modify the example to the default display version and clear the pio version limit

--- example/nes/nes.ino
+++ example/nes/nes.ino
@@ -1,6 +1,6 @@
 /* The product now has two screens, and the initialization code needs a small change in the new version. The LCD_MODULE_CMD_1 is used to define the
  * switch macro. */
-#define LCD_MODULE_CMD_1
+// #define LCD_MODULE_CMD_1
 
 #include <esp_task_wdt.h>
 #include <esp_wifi.h>
@@ -22,7 +22,7 @@ extern "C" {
 #define FSROOT "/fs"
 
 Arduino_DataBus *bus = new Arduino_ESP32LCD8(7 /* DC */, 6 /* CS */, 8 /* WR */, 9 /* RD */, 39 /* D0 */, 40 /* D1 */, 41 /* D2 */, 42 /* D3 */,
-                                             45 /* D4 */, 46 /* D5 */, 47 /* D6 */, 48 /* D7 */);
+        45 /* D4 */, 46 /* D5 */, 47 /* D6 */, 48 /* D7 */);
 Arduino_GFX *gfx = new Arduino_ST7789(bus, 5 /* RST */, 0 /* rotation */, true /* IPS */, 170 /* width */, 320 /* height */, 35 /* col offset 1 */,
                                       0 /* row offset 1 */, 35 /* col offset 2 */, 0 /* row offset 2 */);
 
@@ -31,9 +31,9 @@ extern uint16_t myPalette[];
 int16_t bg_color;
 #if defined(LCD_MODULE_CMD_1)
 typedef struct {
-  uint8_t cmd;
-  uint8_t data[14];
-  uint8_t len;
+    uint8_t cmd;
+    uint8_t data[14];
+    uint8_t len;
 } lcd_cmd_t;
 
 lcd_cmd_t lcd_st7789v[] = {
@@ -55,102 +55,111 @@ lcd_cmd_t lcd_st7789v[] = {
 };
 #endif
 
-void setup() {
-  pinMode(PIN_POWER_ON, OUTPUT);
-  digitalWrite(PIN_POWER_ON, HIGH);
-  ledcSetup(0, 2000, 8);
-  ledcAttachPin(PIN_LCD_BL, 0);
-  ledcWrite(0, 255); /* Screen brightness can be modified by adjusting this parameter. (0-255) */
+void setup()
+{
+    pinMode(PIN_POWER_ON, OUTPUT);
+    digitalWrite(PIN_POWER_ON, HIGH);
+    ledcSetup(0, 2000, 8);
+    ledcAttachPin(PIN_LCD_BL, 0);
+    ledcWrite(0, 255); /* Screen brightness can be modified by adjusting this parameter. (0-255) */
 
-  Serial.begin(115200);
-  Serial.println("Hello T-Display-S3 NES DEMO");
+    Serial.begin(115200);
+    Serial.println("Hello T-Display-S3 NES DEMO");
 
-  // turn off WiFi
-  esp_wifi_deinit();
+    // turn off WiFi
+    esp_wifi_deinit();
 
-  // disable Core 0 WDT
-  TaskHandle_t idle_0 = xTaskGetIdleTaskHandleForCPU(0);
-  esp_task_wdt_delete(idle_0);
+    // disable Core 0 WDT
+    TaskHandle_t idle_0 = xTaskGetIdleTaskHandleForCPU(0);
+    esp_task_wdt_delete(idle_0);
 
-  gfx->begin();
-  gfx->setRotation(1);
-  bg_color = gfx->color565(24, 28, 24); // DARK DARK GREY
+    gfx->begin();
+    gfx->setRotation(1);
+    bg_color = gfx->color565(24, 28, 24); // DARK DARK GREY
 
 #if defined(LCD_MODULE_CMD_1)
-  for (uint8_t i = 0; i < (sizeof(lcd_st7789v) / sizeof(lcd_cmd_t)); i++) {
-    bus->sendCommand(lcd_st7789v[i].cmd);
-    for (int j = 0; j < lcd_st7789v[i].len & 0x7f; j++) {
-      bus->sendData(lcd_st7789v[i].data[j]);
-    }
+    for (uint8_t i = 0; i < (sizeof(lcd_st7789v) / sizeof(lcd_cmd_t)); i++) {
+        bus->sendCommand(lcd_st7789v[i].cmd);
+        for (int j = 0; j < lcd_st7789v[i].len & 0x7f; j++) {
+            bus->sendData(lcd_st7789v[i].data[j]);
+        }
 
-    if (lcd_st7789v[i].len & 0x80) {
-      delay(120);
+        if (lcd_st7789v[i].len & 0x80) {
+            delay(120);
+        }
     }
-  }
 #endif
 
-  gfx->draw16bitRGBBitmap(0, 0, (uint16_t *)img_logo, 320, 172);
-  delay(2000);
+    gfx->draw16bitRGBBitmap(0, 0, (uint16_t *)img_logo, 320, 172);
+    delay(2000);
 
-  SPIFFS.begin(true, FSROOT);
+    SPIFFS.begin(true, FSROOT);
 
-  FS filesystem = SPIFFS;
+    FS filesystem = SPIFFS;
 
-  // find first rom file (*.nes)
-  File root = filesystem.open("/");
-  char *argv[1];
-  if (!root) {
-    Serial.println("Filesystem mount failed!");
-  } else {
-    bool foundRom = false;
+    // find first rom file (*.nes)
+    File root = filesystem.open("/");
+    char *argv[1];
+    if (!root) {
+        Serial.println("Filesystem mount failed!");
+    } else {
+        bool foundRom = false;
 
-    File file = root.openNextFile();
-    while (file) {
-      if (file.isDirectory()) {
-        // skip
-      } else {
-        char *filename = (char *)file.name();
-        int8_t len = strlen(filename);
-        if (strstr(strlwr(filename + (len - 4)), ".nes")) {
-          foundRom = true;
-          char fullFilename[256];
-          sprintf(fullFilename, "%s/%s", FSROOT, filename);
-          Serial.println(fullFilename);
-          argv[0] = fullFilename;
-          break;
+        File file = root.openNextFile();
+        while (file) {
+            if (file.isDirectory()) {
+                // skip
+            } else {
+                char *filename = (char *)file.name();
+                int8_t len = strlen(filename);
+                if (strstr(strlwr(filename + (len - 4)), ".nes")) {
+                    foundRom = true;
+                    char fullFilename[256];
+                    sprintf(fullFilename, "%s/%s", FSROOT, filename);
+                    Serial.println(fullFilename);
+                    argv[0] = fullFilename;
+                    break;
+                }
+            }
+
+            file = root.openNextFile();
         }
-      }
 
-      file = root.openNextFile();
+        if (!foundRom) {
+            Serial.println("Failed to find rom file, please copy rom file to data folder and upload with \"ESP32 Sketch Data Upload\"");
+            argv[0] = "/";
+        }
+
+        Serial.println("NoFrendo start!\n");
+        nofrendo_main(1, argv);
+        Serial.println("NoFrendo end!\n");
     }
+}
+void loop(void)
+{
+    delay(1);
+}
 
-    if (!foundRom) {
-      Serial.println("Failed to find rom file, please copy rom file to data folder and upload with \"ESP32 Sketch Data Upload\"");
-      argv[0] = "/";
+extern "C" void display_init()
+{
+    frame_x = (gfx->width() - NES_SCREEN_WIDTH) / 2;
+    frame_y = 35;
+}
+
+extern "C" void display_write_frame(const uint8_t *data[])
+{
+    gfx->startWrite();
+    bus->writeC8D16D16(0x2A, frame_x, frame_x + NES_SCREEN_WIDTH - 1);
+    bus->writeC8D16D16(0x2B, frame_y, frame_y + NES_SCREEN_HEIGHT - 1);
+    bus->writeCommand(0x2c);
+    for (int32_t i = 0; i < NES_SCREEN_HEIGHT; i++) {
+        if ((i % 7) < 5)
+            bus->writeIndexedPixels((uint8_t *)(data[i]), myPalette, NES_SCREEN_WIDTH);
     }
-
-    Serial.println("NoFrendo start!\n");
-    nofrendo_main(1, argv);
-    Serial.println("NoFrendo end!\n");
-  }
-}
-void loop(void) { delay(1); }
-
-extern "C" void display_init() {
-  frame_x = (gfx->width() - NES_SCREEN_WIDTH) / 2;
-  frame_y = 35;
+    gfx->endWrite();
 }
 
-extern "C" void display_write_frame(const uint8_t *data[]) {
-  gfx->startWrite();
-  bus->writeC8D16D16(0x2A, frame_x, frame_x + NES_SCREEN_WIDTH - 1);
-  bus->writeC8D16D16(0x2B, frame_y, frame_y + NES_SCREEN_HEIGHT - 1);
-  bus->writeCommand(0x2c);
-  for (int32_t i = 0; i < NES_SCREEN_HEIGHT; i++) {
-    if ((i % 7) < 5)
-      bus->writeIndexedPixels((uint8_t *)(data[i]), myPalette, NES_SCREEN_WIDTH);
-  }
-  gfx->endWrite();
+extern "C" void display_clear()
+{
+    gfx->fillScreen(bg_color);
 }
-
-extern "C" void display_clear() { gfx->fillScreen(bg_color); }
--- example/tft/tft.ino
+++ example/tft/tft.ino
@@ -5,7 +5,7 @@
 
 /* The product now has two screens, and the initialization code needs a small change in the new version. The LCD_MODULE_CMD_1 is used to define the
  * switch macro. */
-#define LCD_MODULE_CMD_1
+// #define LCD_MODULE_CMD_1
 
 TFT_eSPI tft = TFT_eSPI();
 #define WAIT 1000
@@ -13,9 +13,9 @@ unsigned long targetTime = 0; // Used for testing draw times
 
 #if defined(LCD_MODULE_CMD_1)
 typedef struct {
-  uint8_t cmd;
-  uint8_t data[14];
-  uint8_t len;
+    uint8_t cmd;
+    uint8_t data[14];
+    uint8_t len;
 } lcd_cmd_t;
 
 lcd_cmd_t lcd_st7789v[] = {
@@ -36,190 +36,192 @@ lcd_cmd_t lcd_st7789v[] = {
 };
 #endif
 
-void setup() {
-  pinMode(PIN_POWER_ON, OUTPUT);
-  digitalWrite(PIN_POWER_ON, HIGH);
+void setup()
+{
+    pinMode(PIN_POWER_ON, OUTPUT);
+    digitalWrite(PIN_POWER_ON, HIGH);
 
-  Serial.begin(115200);
-  Serial.println("Hello T-Display-S3");
+    Serial.begin(115200);
+    Serial.println("Hello T-Display-S3");
 
-  tft.begin();
+    tft.begin();
 
 #if defined(LCD_MODULE_CMD_1)
-  for (uint8_t i = 0; i < (sizeof(lcd_st7789v) / sizeof(lcd_cmd_t)); i++) {
-    tft.writecommand(lcd_st7789v[i].cmd);
-    for (int j = 0; j < lcd_st7789v[i].len & 0x7f; j++) {
-      tft.writedata(lcd_st7789v[i].data[j]);
-    }
+    for (uint8_t i = 0; i < (sizeof(lcd_st7789v) / sizeof(lcd_cmd_t)); i++) {
+        tft.writecommand(lcd_st7789v[i].cmd);
+        for (int j = 0; j < lcd_st7789v[i].len & 0x7f; j++) {
+            tft.writedata(lcd_st7789v[i].data[j]);
+        }
 
-    if (lcd_st7789v[i].len & 0x80) {
-      delay(120);
+        if (lcd_st7789v[i].len & 0x80) {
+            delay(120);
+        }
     }
-  }
 #endif
 
-  tft.setRotation(3);
-  tft.setSwapBytes(true);
-  tft.pushImage(0, 0, 320, 170, (uint16_t *)img_logo);
-  delay(2000);
+    tft.setRotation(3);
+    tft.setSwapBytes(true);
+    tft.pushImage(0, 0, 320, 170, (uint16_t *)img_logo);
+    delay(2000);
 
-  ledcSetup(0, 2000, 8);
-  ledcAttachPin(PIN_LCD_BL, 0);
-  ledcWrite(0, 255);
+    ledcSetup(0, 2000, 8);
+    ledcAttachPin(PIN_LCD_BL, 0);
+    ledcWrite(0, 255);
 }
 
-void loop() {
-  targetTime = millis();
+void loop()
+{
+    targetTime = millis();
 
-  // First we test them with a background colour set
-  tft.setTextSize(1);
-  tft.fillScreen(TFT_BLACK);
-  tft.setTextColor(TFT_GREEN, TFT_BLACK);
+    // First we test them with a background colour set
+    tft.setTextSize(1);
+    tft.fillScreen(TFT_BLACK);
+    tft.setTextColor(TFT_GREEN, TFT_BLACK);
 
-  tft.drawString(" !\"#$%&'()*+,-./0123456", 0, 0, 2);
-  tft.drawString("789:;<=>?@ABCDEFGHIJKL", 0, 16, 2);
-  tft.drawString("MNOPQRSTUVWXYZ[\\]^_`", 0, 32, 2);
-  tft.drawString("abcdefghijklmnopqrstuvw", 0, 48, 2);
-  int xpos = 0;
-  xpos += tft.drawString("xyz{|}~", 0, 64, 2);
-  tft.drawChar(127, xpos, 64, 2);
-  delay(WAIT);
+    tft.drawString(" !\"#$%&'()*+,-./0123456", 0, 0, 2);
+    tft.drawString("789:;<=>?@ABCDEFGHIJKL", 0, 16, 2);
+    tft.drawString("MNOPQRSTUVWXYZ[\\]^_`", 0, 32, 2);
+    tft.drawString("abcdefghijklmnopqrstuvw", 0, 48, 2);
+    int xpos = 0;
+    xpos += tft.drawString("xyz{|}~", 0, 64, 2);
+    tft.drawChar(127, xpos, 64, 2);
+    delay(WAIT);
 
-  tft.fillScreen(TFT_BLACK);
-  tft.setTextColor(TFT_GREEN, TFT_BLACK);
+    tft.fillScreen(TFT_BLACK);
+    tft.setTextColor(TFT_GREEN, TFT_BLACK);
 
-  tft.drawString(" !\"#$%&'()*+,-.", 0, 0, 4);
-  tft.drawString("/0123456789:;", 0, 26, 4);
-  tft.drawString("<=>?@ABCDE", 0, 52, 4);
-  tft.drawString("FGHIJKLMNO", 0, 78, 4);
-  tft.drawString("PQRSTUVWX", 0, 104, 4);
-  delay(WAIT);
+    tft.drawString(" !\"#$%&'()*+,-.", 0, 0, 4);
+    tft.drawString("/0123456789:;", 0, 26, 4);
+    tft.drawString("<=>?@ABCDE", 0, 52, 4);
+    tft.drawString("FGHIJKLMNO", 0, 78, 4);
+    tft.drawString("PQRSTUVWX", 0, 104, 4);
+    delay(WAIT);
 
-  tft.fillScreen(TFT_BLACK);
-  tft.drawString("YZ[\\]^_`abc", 0, 0, 4);
-  tft.drawString("defghijklmno", 0, 26, 4);
-  tft.drawString("pqrstuvwxyz", 0, 52, 4);
-  xpos = 0;
-  xpos += tft.drawString("{|}~", 0, 78, 4);
-  tft.drawChar(127, xpos, 78, 4);
-  delay(WAIT);
+    tft.fillScreen(TFT_BLACK);
+    tft.drawString("YZ[\\]^_`abc", 0, 0, 4);
+    tft.drawString("defghijklmno", 0, 26, 4);
+    tft.drawString("pqrstuvwxyz", 0, 52, 4);
+    xpos = 0;
+    xpos += tft.drawString("{|}~", 0, 78, 4);
+    tft.drawChar(127, xpos, 78, 4);
+    delay(WAIT);
 
-  tft.fillScreen(TFT_BLACK);
-  tft.setTextColor(TFT_BLUE, TFT_BLACK);
+    tft.fillScreen(TFT_BLACK);
+    tft.setTextColor(TFT_BLUE, TFT_BLACK);
 
-  tft.drawString("012345", 0, 0, 6);
-  tft.drawString("6789", 0, 40, 6);
-  tft.drawString("apm-:.", 0, 80, 6);
-  delay(WAIT);
+    tft.drawString("012345", 0, 0, 6);
+    tft.drawString("6789", 0, 40, 6);
+    tft.drawString("apm-:.", 0, 80, 6);
+    delay(WAIT);
 
-  tft.fillScreen(TFT_BLACK);
-  tft.setTextColor(TFT_RED, TFT_BLACK);
+    tft.fillScreen(TFT_BLACK);
+    tft.setTextColor(TFT_RED, TFT_BLACK);
 
-  tft.drawString("0123", 0, 0, 7);
-  tft.drawString("4567", 0, 60, 7);
-  delay(WAIT);
+    tft.drawString("0123", 0, 0, 7);
+    tft.drawString("4567", 0, 60, 7);
+    delay(WAIT);
 
-  tft.fillScreen(TFT_BLACK);
-  tft.drawString("890:.", 0, 0, 7);
-  tft.drawString("", 0, 60, 7);
-  delay(WAIT);
+    tft.fillScreen(TFT_BLACK);
+    tft.drawString("890:.", 0, 0, 7);
+    tft.drawString("", 0, 60, 7);
+    delay(WAIT);
 
-  tft.fillScreen(TFT_BLACK);
-  tft.setTextColor(TFT_YELLOW, TFT_BLACK);
+    tft.fillScreen(TFT_BLACK);
+    tft.setTextColor(TFT_YELLOW, TFT_BLACK);
 
-  tft.drawString("01", 0, 0, 8);
-  delay(WAIT);
+    tft.drawString("01", 0, 0, 8);
+    delay(WAIT);
 
-  tft.drawString("23", 0, 0, 8);
-  delay(WAIT);
+    tft.drawString("23", 0, 0, 8);
+    delay(WAIT);
 
-  tft.drawString("45", 0, 0, 8);
-  delay(WAIT);
+    tft.drawString("45", 0, 0, 8);
+    delay(WAIT);
 
-  tft.drawString("67", 0, 0, 8);
-  delay(WAIT);
+    tft.drawString("67", 0, 0, 8);
+    delay(WAIT);
 
-  tft.drawString("89", 0, 0, 8);
-  delay(WAIT);
+    tft.drawString("89", 0, 0, 8);
+    delay(WAIT);
 
-  tft.drawString("0:.", 0, 0, 8);
-  delay(WAIT);
+    tft.drawString("0:.", 0, 0, 8);
+    delay(WAIT);
 
-  tft.setTextColor(TFT_MAGENTA);
-  tft.drawNumber(millis() - targetTime, 0, 100, 4);
-  delay(4000);
+    tft.setTextColor(TFT_MAGENTA);
+    tft.drawNumber(millis() - targetTime, 0, 100, 4);
+    delay(4000);
 
-  // Now test them with transparent background
-  targetTime = millis();
+    // Now test them with transparent background
+    targetTime = millis();
 
-  tft.setTextSize(1);
-  tft.fillScreen(TFT_BROWN);
-  tft.setTextColor(TFT_GREEN);
+    tft.setTextSize(1);
+    tft.fillScreen(TFT_BROWN);
+    tft.setTextColor(TFT_GREEN);
 
-  tft.drawString(" !\"#$%&'()*+,-./0123456", 0, 0, 2);
-  tft.drawString("789:;<=>?@ABCDEFGHIJKL", 0, 16, 2);
-  tft.drawString("MNOPQRSTUVWXYZ[\\]^_`", 0, 32, 2);
-  tft.drawString("abcdefghijklmnopqrstuvw", 0, 48, 2);
-  xpos = 0;
-  xpos += tft.drawString("xyz{|}~", 0, 64, 2);
-  tft.drawChar(127, xpos, 64, 2);
-  delay(WAIT);
+    tft.drawString(" !\"#$%&'()*+,-./0123456", 0, 0, 2);
+    tft.drawString("789:;<=>?@ABCDEFGHIJKL", 0, 16, 2);
+    tft.drawString("MNOPQRSTUVWXYZ[\\]^_`", 0, 32, 2);
+    tft.drawString("abcdefghijklmnopqrstuvw", 0, 48, 2);
+    xpos = 0;
+    xpos += tft.drawString("xyz{|}~", 0, 64, 2);
+    tft.drawChar(127, xpos, 64, 2);
+    delay(WAIT);
 
-  tft.fillScreen(TFT_BROWN);
-  tft.setTextColor(TFT_GREEN);
+    tft.fillScreen(TFT_BROWN);
+    tft.setTextColor(TFT_GREEN);
 
-  tft.drawString(" !\"#$%&'()*+,-.", 0, 0, 4);
-  tft.drawString("/0123456789:;", 0, 26, 4);
-  tft.drawString("<=>?@ABCDE", 0, 52, 4);
-  tft.drawString("FGHIJKLMNO", 0, 78, 4);
-  tft.drawString("PQRSTUVWX", 0, 104, 4);
+    tft.drawString(" !\"#$%&'()*+,-.", 0, 0, 4);
+    tft.drawString("/0123456789:;", 0, 26, 4);
+    tft.drawString("<=>?@ABCDE", 0, 52, 4);
+    tft.drawString("FGHIJKLMNO", 0, 78, 4);
+    tft.drawString("PQRSTUVWX", 0, 104, 4);
 
-  delay(WAIT);
-  tft.fillScreen(TFT_BROWN);
-  tft.drawString("YZ[\\]^_`abc", 0, 0, 4);
-  tft.drawString("defghijklmno", 0, 26, 4);
-  tft.drawString("pqrstuvwxyz", 0, 52, 4);
-  xpos = 0;
-  xpos += tft.drawString("{|}~", 0, 78, 4);
-  tft.drawChar(127, xpos, 78, 4);
-  delay(WAIT);
+    delay(WAIT);
+    tft.fillScreen(TFT_BROWN);
+    tft.drawString("YZ[\\]^_`abc", 0, 0, 4);
+    tft.drawString("defghijklmno", 0, 26, 4);
+    tft.drawString("pqrstuvwxyz", 0, 52, 4);
+    xpos = 0;
+    xpos += tft.drawString("{|}~", 0, 78, 4);
+    tft.drawChar(127, xpos, 78, 4);
+    delay(WAIT);
 
-  tft.fillScreen(TFT_BROWN);
-  tft.setTextColor(TFT_BLUE);
+    tft.fillScreen(TFT_BROWN);
+    tft.setTextColor(TFT_BLUE);
 
-  tft.drawString("012345", 0, 0, 6);
-  tft.drawString("6789", 0, 40, 6);
-  tft.drawString("apm-:.", 0, 80, 6);
-  delay(WAIT);
+    tft.drawString("012345", 0, 0, 6);
+    tft.drawString("6789", 0, 40, 6);
+    tft.drawString("apm-:.", 0, 80, 6);
+    delay(WAIT);
 
-  tft.fillScreen(TFT_BROWN);
-  tft.setTextColor(TFT_RED);
+    tft.fillScreen(TFT_BROWN);
+    tft.setTextColor(TFT_RED);
 
-  tft.drawString("0123", 0, 0, 7);
-  tft.drawString("4567", 0, 60, 7);
-  delay(WAIT);
+    tft.drawString("0123", 0, 0, 7);
+    tft.drawString("4567", 0, 60, 7);
+    delay(WAIT);
 
-  tft.fillScreen(TFT_BROWN);
-  tft.drawString("890:.", 0, 0, 7);
-  tft.drawString("", 0, 60, 7);
-  delay(WAIT);
+    tft.fillScreen(TFT_BROWN);
+    tft.drawString("890:.", 0, 0, 7);
+    tft.drawString("", 0, 60, 7);
+    delay(WAIT);
 
-  tft.fillScreen(TFT_BROWN);
-  tft.setTextColor(TFT_YELLOW);
+    tft.fillScreen(TFT_BROWN);
+    tft.setTextColor(TFT_YELLOW);
 
-  tft.drawString("0123", 0, 0, 8);
-  delay(WAIT);
+    tft.drawString("0123", 0, 0, 8);
+    delay(WAIT);
 
-  tft.fillScreen(TFT_BROWN);
-  tft.drawString("4567", 0, 0, 8);
-  delay(WAIT);
+    tft.fillScreen(TFT_BROWN);
+    tft.drawString("4567", 0, 0, 8);
+    delay(WAIT);
 
-  tft.fillScreen(TFT_BROWN);
-  tft.drawString("890:.", 0, 0, 8);
-  delay(WAIT);
+    tft.fillScreen(TFT_BROWN);
+    tft.drawString("890:.", 0, 0, 8);
+    delay(WAIT);
 
-  tft.setTextColor(TFT_MAGENTA);
+    tft.setTextColor(TFT_MAGENTA);
 
-  tft.drawNumber(millis() - targetTime, 0, 100, 4);
-  delay(4000);
+    tft.drawNumber(millis() - targetTime, 0, 100, 4);
+    delay(4000);
 }
