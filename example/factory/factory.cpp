02da0b28a32986853db9fac3f98680b4073799fe
RanchoHam <50395146+RanchoHam@users.noreply.github.com>
2025-07-16 08:55:27 -0700

    Update with latest root certificate for https://ipapi.co/timezone/

--- example/factory/factory.cpp
+++ example/factory/factory.cpp
@@ -419,8 +419,21 @@ void setTimezone()
 #ifdef CUSTOM_TIMEZONE
     String timezone = CUSTOM_TIMEZONE;
 #else
-    const char *rootCACertificate =
-        "-----BEGIN CERTIFICATE-----\r\n"\
+    const char *rootCACertificate = 
+        "-----BEGIN CERTIFICATE-----\n"\
+        "MIICCTCCAY6gAwIBAgINAgPlwGjvYxqccpBQUjAKBggqhkjOPQQDAzBHMQswCQYD\n" \
+        "VQQGEwJVUzEiMCAGA1UEChMZR29vZ2xlIFRydXN0IFNlcnZpY2VzIExMQzEUMBIG\n" \
+        "A1UEAxMLR1RTIFJvb3QgUjQwHhcNMTYwNjIyMDAwMDAwWhcNMzYwNjIyMDAwMDAw\n" \
+        "WjBHMQswCQYDVQQGEwJVUzEiMCAGA1UEChMZR29vZ2xlIFRydXN0IFNlcnZpY2Vz\n" \
+        "IExMQzEUMBIGA1UEAxMLR1RTIFJvb3QgUjQwdjAQBgcqhkjOPQIBBgUrgQQAIgNi\n" \
+        "AATzdHOnaItgrkO4NcWBMHtLSZ37wWHO5t5GvWvVYRg1rkDdc/eJkTBa6zzuhXyi\n" \
+        "QHY7qca4R9gq55KRanPpsXI5nymfopjTX15YhmUPoYRlBtHci8nHc8iMai/lxKvR\n" \
+        "HYqjQjBAMA4GA1UdDwEB/wQEAwIBhjAPBgNVHRMBAf8EBTADAQH/MB0GA1UdDgQW\n" \
+        "BBSATNbrdP9JNqPV2Py1PsVq8JQdjDAKBggqhkjOPQQDAwNpADBmAjEA6ED/g94D\n" \
+        "9J+uHXqnLrmvT/aDHQ4thQEd0dlq7A/Cr8deVl5c1RxYIigL9zC2L7F8AjEA8GE8\n" \
+        "p/SgguMh1YQdc4acLa/KNJvxn7kjNuK8YAOdgLOaVsjh4rsUecrNIdSUtUlD\n" 
+        "-----END CERTIFICATE-----\n";
+/*
         "MIIDzTCCArWgAwIBAgIQCjeHZF5ftIwiTv0b7RQMPDANBgkqhkiG9w0BAQsFADBa\r\n"\
         "MQswCQYDVQQGEwJJRTESMBAGA1UEChMJQmFsdGltb3JlMRMwEQYDVQQLEwpDeWJl\r\n"\
         "clRydXN0MSIwIAYDVQQDExlCYWx0aW1vcmUgQ3liZXJUcnVzdCBSb290MB4XDTIw\r\n"\
@@ -442,12 +455,12 @@ void setTimezone()
         "goE6y/SJXQ7vTQ1unBuCJN0yJV0ReFEQPaA1IwQvZW+cwdFD19Ae8zFnWSfda9J1\r\n"\
         "CZMRJCQUzym+5iPDuI9yP+kHyCREU3qzuWFloUwOxkgAyXVjBYdwRVKD05WdRerw\r\n"\
         "6DEdfgkfCv4+3ao8XnTSrLE=\r\n"\
-        "-----END CERTIFICATE-----\r\n";
-
+*/
     WiFiClientSecure *client = new WiFiClientSecure;
     String timezone;
     if (client) {
         client->setCACert(rootCACertificate);
+        Serial.println("checking timezone");
         HTTPClient https;
         if (https.begin(*client, GET_TIMEZONE_API)) {
             int httpCode = https.GET();
